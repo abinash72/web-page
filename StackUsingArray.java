2c7b3f1619aa4ca72569c2c06a92d53672b243be
Abinash Sahu <107759499+abinash72@users.noreply.github.com>
2023-10-25 20:32:13 +0530

    Add files via upload

---
+++ StackUsingArray.java
@@ -0,0 +1,12 @@
+//class Stack{
+//    private int maxsize;
+//    private int [] stackArray;
+//    private int top;
+//}
+//public Stack(int size){
+//  this.maxsize=size;
+//this.
+//}
+//public class StackUsingArray {
+//
+//}
